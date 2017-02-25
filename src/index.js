38ed432693eb740ebf8022cbe13e34def0f40e4b
Chris Atkin <atkinchris@gmail.com>
2017-02-25 15:19:47 +0000

    Basic mouse shader

--- src/index.js
+++ src/index.js
@@ -1,32 +1,43 @@
 import 'pixi.js'
 import shader from './shader.frag'
 
-const app = new PIXI.Application(1200, 800, { backgroundColor: 0x1099bb })
+const { WebGLRenderer, Container, loader, Sprite } = PIXI
+const { innerWidth, innerHeight } = window
 
-document.body.appendChild(app.view)
+const renderer = new WebGLRenderer(innerWidth, innerHeight, { backgroundColor: 0x1099bb })
+document.body.appendChild(renderer.view)
 
-const { loader, Sprite } = PIXI
+const stage = new Container()
+const uniforms = {
+  mouse: { type: 'v2', value: { x: 0, y: 0 } },
+  time: { type: 'f', value: 0 },
+  resolution: { type: 'v2', value: { x: innerWidth, y: innerHeight } },
+}
+const simpleShader = new PIXI.Filter('', shader, uniforms)
 
 loader.baseUrl = 'assets'
 loader.add('road', 'road.png')
 
-console.log(shader)
-
 loader.load((ldr, resources) => {
   const road = new Sprite(resources.road.texture)
-  const simpleShader = new PIXI.AbstractFilter('', shader)
 
   road.filters = [simpleShader]
-  road.x = app.renderer.width / 2
-  road.y = app.renderer.height / 2
+  road.x = renderer.width / 2
+  road.y = renderer.height / 2
   road.anchor.x = 0.5
   road.anchor.y = 0.5
 
-  app.stage.addChild(road)
+  stage.addChild(road)
 })
 
 function animate() {
   requestAnimationFrame(animate)
-  app.render(app.stage)
+  renderer.render(stage)
+  simpleShader.uniforms.time += 0.1
 }
 animate()
+
+document.onmousemove = function (evt) {
+  simpleShader.uniforms.mouse.x = evt.clientX
+  simpleShader.uniforms.mouse.y = evt.clientY
+}
--- src/shader.frag
+++ src/shader.frag
@@ -1,3 +1,9 @@
+precision mediump float;
+
+uniform vec2 mouse;
+uniform vec2 resolution;
+uniform float time;
+
 void main(){
-   gl_FragColor = vec4(gl_FragCoord.x/1000.0,0.0,0.0,1.0);
+  gl_FragColor = vec4(sin(time), mouse.x/resolution.x, mouse.y/resolution.y, 1.0);
 }
