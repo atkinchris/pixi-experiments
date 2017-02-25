24bcb8f4843f4fda3e9e5f01fb72fb59fbd941b0
Chris Atkin <atkinchris@gmail.com>
2017-02-25 18:20:19 +0000

    Change to rock texture

--- src/index.js
+++ src/index.js
@@ -2,26 +2,27 @@ import 'pixi.js'
 import shader from './shader.frag'
 
 const { WebGLRenderer, Container, loader, Sprite } = PIXI
-const { innerWidth, innerHeight } = window
+const width = 300
+const height = 225
 
-const renderer = new WebGLRenderer(innerWidth, innerHeight, { backgroundColor: 0x1099bb })
+const renderer = new WebGLRenderer(width, height)
 document.body.appendChild(renderer.view)
 
 const stage = new Container()
 const uniforms = {
   mouse: { type: 'v2', value: { x: 0, y: 0 } },
   time: { type: 'f', value: 0 },
-  resolution: { type: 'v2', value: { x: innerWidth, y: innerHeight } },
+  resolution: { type: 'v2', value: { x: width, y: height } },
 }
 const simpleShader = new PIXI.Filter('', shader, uniforms)
 
 loader.baseUrl = 'assets'
-loader.add('road', 'road.png')
+loader.add('rock', 'rock.png')
 
 loader.load((ldr, resources) => {
-  const road = new Sprite(resources.road.texture)
+  const road = new Sprite(resources.rock.texture)
 
-  road.filters = [simpleShader]
+  // road.filters = [simpleShader]
   road.x = renderer.width / 2
   road.y = renderer.height / 2
   road.anchor.x = 0.5
