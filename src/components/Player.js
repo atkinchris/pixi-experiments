7286e9469a48b66b83e0cec3f441a5918655f2e0
Chris Atkin <atkinchris@gmail.com>
2017-04-20 19:52:20 +0100

    Round movement to prevent subpixel stuttering

--- src/components/Player.js
+++ src/components/Player.js
@@ -5,39 +5,45 @@ class Player extends PIXI.Sprite {
   constructor(x, y) {
     super(PIXI.loader.resources.player.texture)
 
+    this.posX = x
+    this.posY = y
     this.x = x
     this.y = y
+
+
+    this.minX = 0 - this.width
+    this.minY = 0 - this.height
+    this.speed = 30
 
     this.inputHandler = setupInputHandler()
   }
 
   update(dt) {
-    const speed = 30
     const direction = this.inputHandler()
     const heading = directionToVector(direction)
 
     if (!isNaN(dt)) {
-      this.x += dt * speed * heading.x
-      this.y -= dt * speed * heading.y
-    }
+      this.posX += dt * this.speed * heading.x
+      this.posY -= dt * this.speed * heading.y
 
-    const minX = 0 - this.width
-    const minY = 0 - this.height
+      this.x = Math.round(this.posX)
+      this.y = Math.round(this.posY)
 
-    if (this.x < minX) {
-      this.x = 320
-    }
+      if (this.posX < this.minX) {
+        this.posX = 320
+      }
 
-    if (this.x > 320) {
-      this.x = minX
-    }
+      if (this.posX > 320) {
+        this.posX = this.minX
+      }
 
-    if (this.y < minY) {
-      this.y = 320
-    }
+      if (this.posY < this.minY) {
+        this.posY = 320
+      }
 
-    if (this.y > 320) {
-      this.y = minY
+      if (this.posY > 320) {
+        this.posY = this.minY
+      }
     }
   }
 }
