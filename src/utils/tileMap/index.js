8ab284fc5efe589db13f8c7d0233d33662a67397
Chris Atkin <atkinchris@gmail.com>
2017-05-01 19:29:43 +0100

    Update tile map to have adjacents array

--- src/utils/tileMap/index.js
+++ src/utils/tileMap/index.js
@@ -1,29 +1,41 @@
 import arrayToMap from './arrayToMap'
 import mapData from './map.json'
 
+import { LEFT, RIGHT, UP, DOWN } from '../directions'
+
 const TEST_MAP = arrayToMap(mapData)
+const impassable = ({ x, y }) => ({ x, y, passable: false, adjacents: [] })
 
-function createTileMap(map = TEST_MAP) {
-  function getTile(coordinates) {
-    const x = Math.round(coordinates.x)
-    const y = Math.round(coordinates.y)
+function getTile(map, coordinates) {
+  const x = Math.round(coordinates.x)
+  const y = Math.round(coordinates.y)
 
-    return map.find(t => t.x === x && t.y === y) || { passable: false, x, y }
-  }
+  return map.find(t => t.x === x && t.y === y) || impassable(coordinates)
+}
 
-  function getAdjacentTile(tile, direction) {
-    const x = tile.x + direction.x
-    const y = tile.y + direction.y
+function getAdjacentTile(map, coordinates, direction) {
+  const x = coordinates.x + direction.x
+  const y = coordinates.y + direction.y
 
-    return getTile({ x, y })
-  }
+  return getTile(map, { x, y })
+}
 
-  const each = fn => map.map(fn)
+function createTileMap(mapObjects = TEST_MAP) {
+  const map = mapObjects.map((tile) => {
+    const adjacents = [
+      { ...getAdjacentTile(mapObjects, tile, UP), direction: UP },
+      { ...getAdjacentTile(mapObjects, tile, LEFT), direction: LEFT },
+      { ...getAdjacentTile(mapObjects, tile, RIGHT), direction: RIGHT },
+      { ...getAdjacentTile(mapObjects, tile, DOWN), direction: DOWN },
+    ]
+
+    return { ...tile, adjacents }
+  })
 
   return {
-    each,
-    getAdjacentTile,
-    getTile,
+    each: fn => map.map(fn),
+    getTile: coordinates => getTile(map, coordinates),
+    getAdjacentTile: (coordinates, direction) => getAdjacentTile(map, coordinates, direction),
   }
 }
 
